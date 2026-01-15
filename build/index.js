7f97ce21115ae720a8429b84a89ff34f43dd128a
Nahid Monsur Majumder <nahid.majumder46@gmail.com>
2026-01-15 14:14:08 +0600

    Add files via upload

---
+++ build/index.js
@@ -0,0 +1,21 @@
+const decreasebtn=document.getElementById("decreasebtn");
+const resetbtn=document.getElementById("resetbtn");
+const increasebtn=document.getElementById("increasebtn");
+const countable=document.getElementById("countable");
+let count=0;
+increasebtn.onclick=function()
+{
+    count++;
+    countable.textContent=count;
+}
+decreasebtn.onclick=function()
+{
+    count--;
+    countable.textContent=count;
+}
+resetbtn.onclick=function()
+{
+    count=0;
+    countable.textContent=count;
+}
+
